99da3ae98ccdb61419661c9a5b89040c2b7254a3
Manuel <antdev1987@gmail.com>
2023-01-02 10:55:57 -0600

    conect db generate sample products and users

---
+++ routes/productRoutes.js
@@ -0,0 +1,55 @@
+import express from 'express'
+import Product from '../models/productModel.js'
+
+const productRoutes = express.Router()
+
+
+//fetching all the products
+productRoutes.get('/', async(req,res)=>{
+    console.log('aqui')
+
+    const products = await Product.find()
+    res.send(products)
+
+})
+
+
+//to fetch a single product
+productRoutes.get('/slug/:slug', async(req, res) => {
+    const { slug } = req.params
+
+    try {
+        const product = await Product.findOne({slug})
+        if (product) {
+            res.send(product)
+        } else {
+            res.status(404).send({ message: 'no encontrado' })
+            // throw new Error({message:'no encontrado'})
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+
+})
+
+
+// //to check count in stock
+productRoutes.get('/:id', async (req, res) => {
+    const { id } = req.params
+    console.log(id)
+    const product = await Product.findById(id)
+
+    const stock = product.countInStock
+
+    if (product) {
+        res.json({ stock })
+    } else {
+        res.sendStatus(404).send({ message: 'no encontrado' })
+        // throw new Error({message:'no encontrado'})
+    }
+
+})
+
+export default productRoutes
--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 import express from 'express'
-import data from './data.js'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
+import seedRouter from './routes/seedRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+
+dotenv.config()
 const app = express()
 
 
-app.get('/api/products',(req,res)=>{
-    res.send(data.products)
-})
+app.use(cors())
+
+//connecting to db
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('connection done')
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
 
-const port = process.env.PORT || 5000
+//creating sample
+app.use('/api/seed', seedRouter)
 
-app.listen(port, ()=>{
+
+//to fetch all the products
+app.use('/api/products', productRoutes)
+
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {
     console.log(`server at http://localhost:${port}`)
 })
